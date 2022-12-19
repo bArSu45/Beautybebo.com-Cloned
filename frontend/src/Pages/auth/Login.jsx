ea1636f9895c55c4d97a825d64c6d701c5f8136a
abhijitnr <abhijitbiswas8859@gmail.com>
2022-12-19 08:48:52 +0530

    login updated

--- frontend/src/Pages/auth/Login.jsx
+++ frontend/src/Pages/auth/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
   const handleLogin = (event) => {
     event.preventDefault();
     if (email !== "" && password !== "") {
-      setload(true)
+      setload(true);
       axios
         .post("https://repulsive-nightgown-colt.cyclic.app/users/login", {
           email,
@@ -22,7 +22,7 @@ function Login() {
         })
         .then((res) => {
           window.alert("Login successful");
-          setload(false)
+          setload(false);
           //   console.log(res.data.loginUser.accessToken);
           SetLocal("auth", res.data.loginUser.accessToken);
           if (res.data.loginUser.isAdmin) {
@@ -43,55 +43,64 @@ function Login() {
 
   return (
     <div className="login_parent">
-      {Load ? <Loading/> :  
-      <div className="existing_and_new_users">
-        <div className="existing_customer">
-          <p className="existing_user_tag">Existing Customers</p>
+      {Load ? (
+        <Loading />
+      ) : (
+        <div className="existing_and_new_users">
+          <div className="existing_customer">
+            <p className="existing_user_tag">Existing Customers</p>
 
-          <div className="login_form">
-            <form onSubmit={handleLogin}>
-              {/* For email */}
-              <p>
-                <label>
-                  * Email address
-                  <input
-                    required
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    type="email"
-                  />
-                </label>
-              </p>
+            <div className="login_form">
+              <form onSubmit={handleLogin}>
+                {/* For email */}
+                <p>
+                  <label>
+                    * Email address
+                    <br />
+                    <input
+                      className="input_login"
+                      required
+                      value={email}
+                      onChange={(event) => setEmail(event.target.value)}
+                      type="email"
+                    />
+                  </label>
+                </p>
 
-              {/* Form password */}
-              <p>
-                <label>
-                  * Password
-                  <input
-                    type="password"
-                    required
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                  />
-                </label>
-              </p>
+                {/* Form password */}
+                <p>
+                  <label>
+                    * Password
+                    <br />
+                    <input
+                      className="input_login"
+                      type="password"
+                      required
+                      value={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                    />
+                  </label>
+                </p>
 
-              <p className="forgot_password">FORGOTTEN YOUR PASSWORD?</p>
+                <p className="forgot_password">FORGOTTEN YOUR PASSWORD?</p>
 
-              <p>
-                <button className="login_button">LOGIN TO YOUR ACCOUNT</button>
-              </p>
-            </form>
+                <p>
+                  <button className="login_button">
+                    LOGIN TO YOUR ACCOUNT
+                  </button>
+                </p>
+              </form>
+            </div>
+          </div>
+
+          <div className="new_customers">
+            <p className="new_customes_tag">New Customers</p>
+            <Link to="/signup">
+              <button className="continue_to_register">CONTINUE</button>
+            </Link>
           </div>
         </div>
-
-        <div className="new_customers">
-          <p className="new_customes_tag">New Customers</p>
-          <Link to="/signup">
-            <button className="continue_to_register">CONTINUE</button>
-          </Link>
-        </div>
-      </div>}
+      )}
     </div>
   );
 }
